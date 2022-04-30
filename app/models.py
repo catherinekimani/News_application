62653cbc1fe3cfb2909ecea969ec956b5adb9229
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-01 01:59:44 +0300

    Create articles class

--- app/models.py
+++ app/models.py
@@ -11,3 +11,19 @@ class Source:
         self.category = category
         self.language = language
         self.country = country
+        
+class Articles:
+    '''
+    Articles class to define articles objects
+    '''
+    
+    def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt,content):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
