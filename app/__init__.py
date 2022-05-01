c8cb2d2818630db78f575f8bef694284f6e36052
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-01 15:42:17 +0300

    Add Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
+from ensurepip import bootstrap
 from flask import Flask
 from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     
@@ -12,6 +16,8 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
+    # initializing bootstrap
+    bootstrap.init_app(app)
     # setting config
     from .requests import configure_request
     configure_request(app)
