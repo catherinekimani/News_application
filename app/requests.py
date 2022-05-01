50ec494ce5bea06d59e5f0d0254d7489bd20c447
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-01 13:03:10 +0300

    Import articles class

--- app/requests.py
+++ app/requests.py
@@ -1,8 +1,9 @@
 from distutils.command.config import config
-from turtle import back
+from turtle import back, title
 from unicodedata import category
 import urllib.request,json
-from .models import Source #importing source class
+from .models import Source,Articles #importing source class and articles class
+
 
 # getting api key
 api_key = None
@@ -10,10 +11,14 @@ api_key = None
 # getting the news source url
 base_url = None
 
+# getting the articles url
+articles_url = None
+
 def configure_request(app):
     global api_key,base_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
+    articles_url = app.config['NEWS_ARTICLES_BASE_URL']
     
 def get_sources():
     '''
@@ -56,5 +61,47 @@ def process_results(source_list):
             source_results.append(source_object)
             
     return source_results
-        
+
+def get_articles(id):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = articles_url.format(id,api_key)
     
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+        
+        articles_results = None
+    
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_results(articles_results_list)
+            
+    return articles_results
+
+def process_results(articles_list):
+    '''
+    Function that processes the articles result and transform them to a list of objects
+        Args:
+            articles_list: List of dictionaries that contain articles details
+        Returns:
+            articles_results: A list of artcicles objects
+    '''
+    articles_results = []
+    for article_item in articles_list:
+        id = article_item.get('id')
+        name = article_item.get('name')
+        author = article_item.get('author')
+        title = article_item.get('title')
+        description = article_item.get('description')
+        url = article_item.get('url')
+        urlToImage = article_item.get('urlToImage')
+        publishedAt = article_item.get('publishesAt')
+        content = article_item.get('content')
+        
+        if urlToImage:
+            articles_object = Articles(id,name,author,title,description,url,urlToImage,publishedAt,content)
+            articles_results.append(articles_object)
+
+    return articles_results
