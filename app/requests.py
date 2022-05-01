6f0a1728ba498257bbc2ed02c3c7150cfd5d3c7e
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-01 15:31:15 +0300

    Add slight changes

--- app/requests.py
+++ app/requests.py
@@ -15,7 +15,7 @@ base_url = None
 articles_url = None
 
 def configure_request(app):
-    global api_key,base_url
+    global api_key,base_url,articles_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
     articles_url = app.config['NEWS_ARTICLES_BASE_URL']
@@ -34,11 +34,10 @@ def get_sources():
         
         if get_sources_response['sources']:
             source_results_list = get_sources_response['sources']
-            source_results = process_results(source_results_list)
-            
+            source_results = process_news_results(source_results_list)
     return source_results
         
-def process_results(source_list):
+def process_news_results(source_lists):
     '''
     Function that processes the source result and transform them to a list of objects
         Args:
@@ -46,16 +45,17 @@ def process_results(source_list):
         Returns:
             source_results: A list of source objects
     '''
+
     source_results = []
-    for source_item in source_list:
+    for source_item in source_lists:
         id = source_item.get('id')
         name = source_item.get('name')
         description = source_item.get('description')
-        url = source_item.get('url_path')
+        url = source_item.get('url')
         category = source_item.get('category')
         language = source_item.get('language')
         country = source_item.get('country')
-        
+
         if url:
             source_object = Source(id,name,description,url,category,language,country)
             source_results.append(source_object)
@@ -71,16 +71,15 @@ def get_articles(id):
     with urllib.request.urlopen(get_articles_url) as url:
         get_articles_data = url.read()
         get_articles_response = json.loads(get_articles_data)
-        
         articles_results = None
     
         if get_articles_response['articles']:
             articles_results_list = get_articles_response['articles']
-            articles_results = process_results(articles_results_list)
-            
+            articles_results = process_articles_results(articles_results_list)
+
     return articles_results
 
-def process_results(articles_list):
+def process_articles_results(articles_lists):
     '''
     Function that processes the articles result and transform them to a list of objects
         Args:
@@ -89,7 +88,7 @@ def process_results(articles_list):
             articles_results: A list of artcicles objects
     '''
     articles_results = []
-    for article_item in articles_list:
+    for article_item in articles_lists:
         id = article_item.get('id')
         name = article_item.get('name')
         author = article_item.get('author')
