5f1b85db90ebc449f010669f58f76579ec1c6814
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-01 12:15:51 +0300

    Add articles URL

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
     NEWS_API_KEY = 'ffd927c5dc134723b1076da641194ccf'
 
 class ProdConfig(Config):
